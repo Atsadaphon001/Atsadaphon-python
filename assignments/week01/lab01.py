af5e7422b4870d029028ae24c722d484971f17c6
Atsadaphon Buasod <Atsadaphon.b@ku.th>
2025-07-10 15:06:38 +0700

    work1

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,22 @@
+print("=== Personal Profile Creator ===")
+# Create a program that asks for:
+# - Full name
+# - Age
+# - Email
+# - Phone number
+# - Favorite hobby
+# Then display it as a profile
+
+print("=== Personal Profile Creator ===")
+
+name = input("What is Foll Name =")
+age = input('What is your Age = ')
+email =input('What Is Your Email = ')
+phone =input('what is phone number = ')
+hobby =input('Favorite hobby = ')
+
+print('Full Name =', name)
+print('Age =', age)
+print('Email =', email)
+print('phon number =', phone)
+print('Favorite hobby =', hobby)
